14bb38a0c38c2dae35a34ad1e714af8199519016
Kelwin Joanes <kjoanes@gmail.com>
2016-03-25 17:03:31 -0230

    updated login

---
+++ src/chat.php
@@ -0,0 +1,14 @@
+<?php
+	
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Chat</title>
+	</head>
+	<body>
+		<h1>Welcome</h1>
+	</body>
+</html>
+
--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php
-	session_start();
+	include('login.php'); // Includes Login Script
 ?>
 
 <!DOCTYPE html>
@@ -11,7 +11,7 @@
 	<body>
 		<ul>
 			<li class="header"><a href="index.php">CHATTY</a></li>
-			<li class="right"><a href="registration.php">Sign Up</a></li>
+			<li class="right"><a href="registration.html">Sign Up</a></li>
 			<li class="right"><a href="index.php">Sign In</a></li>
 		</ul>
 		<br />
@@ -38,51 +38,10 @@
 				</tr>
 			</table>
 		</form>
+
+		<?php echo "<div class=\"error\">$error</div>"; ?>
+
 		<br />
-		<div id="signup">Don't have an account? <a href="registration.php">Sign up</a>!</div>
-
-
-		<!-- php file to process from data -->
-		<?php
-
-		    $error = "";                // error message
-		    $uname_error = "";          // error message if username does not exist
-		    $pwd_error = "";            // error message if password does not match username
-
-		    if (isset($_POST['submit'])) {
-		        if (empty($_POST['username']) || empty($_POST['password'])) {
-		            $error = "Please provide your username or password";
-		        }
-		        else {
-		            $username=$_POST['uname'];
-		            $password=$_POST['pwd'];
-		            $connection = mysql_connect("localhost", "root", "");   // connect to server
-
-		            // prevent MySQL injection by h@ck3rz
-		            $username = stripslashes($username);  
-		            $password = stripslashes($password);
-		            $username = mysql_real_escape_string($username);
-		            $password = mysql_real_escape_string($password);
-		            
-		            $db = mysql_select_db("company", $connection);  // select database
-		            
-		            $query = mysql_query("select * from login where password='$password' AND username='$username'", $connection);   // query to get info of registered users
-		            $rows = mysql_num_rows($query);
-		            if ($rows == 1) {
-		                $_SESSION['login_user']=$username; // initialize session
-		               // header("location: profile.php"); // go to profile page (or chat page?)
-		                echo 'success!';  // debug
-		            } 
-		            else {
-		                $error = "Username or password is invalid";
-		            }
-		            mysql_close($connection); // close connection
-		        }
-		    }
-
-
-		?>	
-
-
+		<div id="signup">Don't have an account? <a href="registration.html">Sign up</a>!</div>
 	</body>
 </html>
