2d8c1f6229b385d83a59771bd1493c94ac4c63aa
Kelwin Joanes <kjoanes@gmail.com>
2016-03-26 15:37:47 -0230

    fixed reg.php

--- src/reg.php
+++ src/reg.php
@@ -28,7 +28,7 @@
 			$pass = $_POST['password'];
 
 			$sql = "INSERT INTO Info VALUES('','$fname','$lname','$mail','$num','$sex','$dob','$country', '$uname','$pass',0);";
-			//$result = $conn->query($sql);
+			$result = $conn->query($sql);
 
 			if($conn->query($sql) === TRUE)
 			{
