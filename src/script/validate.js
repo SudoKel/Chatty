45c20c4018206a5d48c7876ffe147e45d6843214
ToJen <tomisinjenrola@gmail.com>
2016-03-27 23:10:57 -0230

    Fixed reset button

--- src/script/validate.js
+++ src/script/validate.js
@@ -1,117 +1,138 @@
 $(document).ready(function() {
+
 	$("#submit").click(function(e) {
 
-	e.preventDefault();
+		e.preventDefault();
 
-	var fName = document.getElementById("fName").value;
-	var sName = document.getElementById("sName").value;
-	var email = document.getElementById("email").value;
-	var pNum = document.getElementById("pNum").value;
-	var gender = document.getElementsByClassName("gender");
-	var day = document.getElementById("day");
-	var month = document.getElementById("month");
-	var year = document.getElementById("year");
-	var country = document.getElementById("country");
-	var username = document.getElementById("uname").value;
-	var password = document.getElementById("pwd").value;
+		var fName = document.getElementById("fName").value;
+		var sName = document.getElementById("sName").value;
+		var email = document.getElementById("email").value;
+		var pNum = document.getElementById("pNum").value;
+		var gender = document.getElementsByClassName("gender");
+		var day = document.getElementById("day");
+		var month = document.getElementById("month");
+		var year = document.getElementById("year");
+		var country = document.getElementById("country");
+		var username = document.getElementById("uname").value;
+		var password = document.getElementById("pwd").value;
 
-	var dsel = day[day.selectedIndex].value;
-	var msel = month[month.selectedIndex].value;
-	var ysel = year[year.selectedIndex].value;
-	var csel = country[country.selectedIndex].value;
+		var dsel = day[day.selectedIndex].value;
+		var msel = month[month.selectedIndex].value;
+		var ysel = year[year.selectedIndex].value;
+		var csel = country[country.selectedIndex].value;
 
-	var filled = true;
+		var filled = true;
 
 
-	// check if fields are empty [all fields are required]
-	if(fName=="" || fName==null) {
-		document.getElementById("fErr").innerHTML = "*Your first name is required.";
-		document.getElementById("fErr").style.display = "inline";
-		filled = false;
-	}
+		// check if fields are empty [all fields are required]
+		if(fName=="" || fName==null) {
+			document.getElementById("fErr").innerHTML = "*Your first name is required.";
+			document.getElementById("fErr").style.display = "inline";
+			filled = false;
+		}
 
-	if(sName=="" || sName==null) {
-		document.getElementById("sErr").innerHTML = "*Your last name is required.";
-		document.getElementById("sErr").style.display = "inline";
-		filled = false;
-	}
+		if(sName=="" || sName==null) {
+			document.getElementById("sErr").innerHTML = "*Your last name is required.";
+			document.getElementById("sErr").style.display = "inline";
+			filled = false;
+		}
 
-	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	if(re.test(email)==false) {
-		document.getElementById("eErr").innerHTML = "*Please specify a valid email.";
-		document.getElementById("eErr").style.display = "inline";
-		filled = false;
-	}
-	if(email=="" || email==null) {
-		document.getElementById("eErr").innerHTML = "*Your email is required.";
-		document.getElementById("eErr").style.display = "inline";
-		filled = false;
-	}
+		var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		if(re.test(email)==false) {
+			document.getElementById("eErr").innerHTML = "*Please specify a valid email.";
+			document.getElementById("eErr").style.display = "inline";
+			filled = false;
+		}
+		if(email=="" || email==null) {
+			document.getElementById("eErr").innerHTML = "*Your email is required.";
+			document.getElementById("eErr").style.display = "inline";
+			filled = false;
+		}
 
-	if(pNum=="" || pNum==null) {
-		document.getElementById("pErr").innerHTML = "*Your phone number is required.";
-		document.getElementById("pErr").style.display = "inline";
-		filled = false;
-	}
+		if(pNum=="" || pNum==null) {
+			document.getElementById("pErr").innerHTML = "*Your phone number is required.";
+			document.getElementById("pErr").style.display = "inline";
+			filled = false;
+		}
 
-	if(isNaN(pNum)) {
-		document.getElementById("pErr").innerHTML = "*You need to specify a number.";
-		document.getElementById("pErr").style.display = "inline";
-		filled = false;
-	}
+		if(isNaN(pNum)) {
+			document.getElementById("pErr").innerHTML = "*You need to specify a number.";
+			document.getElementById("pErr").style.display = "inline";
+			filled = false;
+		}
 
-	if(!gender[0].checked && !gender[1].checked && !gender[2].checked) {
-		document.getElementById("gErr").innerHTML = "*Please select an option.";
-		document.getElementById("gErr").style.display = "inline";
-		filled = false;
-	}
+		if(!gender[0].checked && !gender[1].checked && !gender[2].checked) {
+			document.getElementById("gErr").innerHTML = "*Please select an option.";
+			document.getElementById("gErr").style.display = "inline";
+			filled = false;
+		}
 
-	if(dsel=="" || msel=="" || ysel=="") {
-		document.getElementById("dErr").innerHTML = "*Please provide a complete date.";
-		document.getElementById("dErr").style.display = "inline";
-		filled = false;
-	}
+		if(dsel=="" || msel=="" || ysel=="") {
+			document.getElementById("dErr").innerHTML = "*Please provide a complete date.";
+			document.getElementById("dErr").style.display = "inline";
+			filled = false;
+		}
 
-	if(csel=="") {
-		document.getElementById("cErr").innerHTML = "*Please select a country.";
-		document.getElementById("cErr").style.display = "inline";
-		filled = false;
-	}
+		if(csel=="") {
+			document.getElementById("cErr").innerHTML = "*Please select a country.";
+			document.getElementById("cErr").style.display = "inline";
+			filled = false;
+		}
 
-	if(username=="") {
-		document.getElementById("uErr").innerHTML = "*Please create a username.";
-		document.getElementById("uErr").style.display = "inline";
-		filled = false;
-	}
-	
-	if(password=="") {
-		document.getElementById("pdErr").innerHTML = "*Please create a password.";
-		document.getElementById("pdErr").style.display = "inline";
-		filled = false;
-	}
+		if(username=="") {
+			document.getElementById("uErr").innerHTML = "*Please create a username.";
+			document.getElementById("uErr").style.display = "inline";
+			filled = false;
+		}
+		
+		if(password=="") {
+			document.getElementById("pdErr").innerHTML = "*Please create a password.";
+			document.getElementById("pdErr").style.display = "inline";
+			filled = false;
+		}
 
-	if(filled)
-	{
-		if(gender[0].checked)
-			var sex = "Male";
-		else if(gender[1].checked)
-			var sex = "Female";
-		else
-			var sex = "Other";
+		if(filled)
+		{
+			if(gender[0].checked)
+				var sex = "Male";
+			else if(gender[1].checked)
+				var sex = "Female";
+			else
+				var sex = "Other";
 
-		$.ajax({
-			type: "POST",
-			url:  "reg.php",
-			data: {first_name: fName, last_name: sName, email: email, phone_num: pNum, 
-				   gender: sex, DOBYear: ysel, DOBMonth: msel, DOBDay: dsel, 
-				   country: csel, username: username, password: password},
-			success: function(data){
-				console.log(data);
-				if(data == 'true')
-					window.location = "index.php";
-			}
-		});
-	}
+			$.ajax({
+				type: "POST",
+				url:  "reg.php",
+				data: {first_name: fName, last_name: sName, email: email, phone_num: pNum, 
+					   gender: sex, DOBYear: ysel, DOBMonth: msel, DOBDay: dsel, 
+					   country: csel, username: username, password: password},
+				success: function(data){
+					console.log(data);
+					if(data == 'true')
+						window.location = "index.php";
+				}
+			});
+		}
 
 	});
+
+	$("#reset").click(function(f) {
+
+		// f.preventDefault();
+
+		document.getElementById("fErr").style.display = "none";
+		document.getElementById("sErr").style.display = "none";
+		document.getElementById("eErr").style.display = "none";
+		document.getElementById("eErr").style.display = "none";
+		document.getElementById("pErr").style.display = "none";
+		document.getElementById("pErr").style.display = "none";
+		document.getElementById("gErr").style.display = "none";
+		document.getElementById("dErr").style.display = "none";
+		document.getElementById("cErr").style.display = "none";
+		document.getElementById("uErr").style.display = "none";
+		document.getElementById("pdErr").style.display = "none";
+
+
+	});
+
 });
