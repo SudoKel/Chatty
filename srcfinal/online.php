f2c6b4b7644f96a656e68811e20e6e8e68ef20ab
Saahil Budhrani <Saahil@s1022n88h074.wst6.mun.ca>
2016-03-29 03:36:37 -0230

    new src folder with final changes to project

---
+++ srcfinal/online.php
@@ -0,0 +1,31 @@
+<?php 
+	// variables for sql connection 
+	$servername = "mysql.cs.mun.ca";
+	$username = "cs3715_kssj13";
+	$password = "orlando1";
+	$database = "cs3715_kssj13";
+
+	// establish sql connection
+	$conn = new mysqli($servername, $username, $password, $database);
+
+	// select all online users
+	$sql = "select username from Info where online = 1;";
+
+	// run and save the query
+	$result = $conn->query($sql);
+	
+	// if at least 1 user is online then return the username
+	if($result->num_rows > 0)
+	{
+		echo "<div class=\"title\">Online:</div>";
+		// output data of each row
+	    while($row = $result->fetch_assoc()) 
+	    {
+	        echo "<span class=\"onlineUser\">" . $row["username"] . "</span>";
+	        echo "<br />";
+	    }
+	}
+
+	// close sql connection
+	$conn->close();
+?>
---
+++ srcfinal/script/chat.js
@@ -0,0 +1,98 @@
+// file: chat.js
+
+// when document is loaded run the following
+$(document).ready(function(){
+	// SEND MESSAGES
+	// attach function to execute on click of submit button
+	$("#submit").click(function(e){
+		// prevent form from submitting data and resetting fields
+		e.preventDefault();
+		// retrieve chat message from text field
+		var msg = $("#chat").val();
+		// reset text field value
+		$("#chat").val("");
+		// use ajax to send the chat message to post.php
+	 	$.ajax({
+			type: "POST",
+		 	url:  "post.php",
+		 	data: {chat: msg},
+		 	success: function(data)
+		 	{
+		 		// update the chat area with data from post.php
+		 		$("#chatbox").html(data);
+			}
+	 	});
+	});
+				
+	// UPLOAD IMAGES
+	// attach function to execute on click of upload button
+	$("#upload").click(function(e){
+		// prevent form from submitting data and resetting fields
+		e.preventDefault();
+		// retrieve the upload form
+		var form = $("form")[1];
+		// retrieve the data of the upload form
+		var formData = new FormData(form);
+		// use ajax to send the image data to upload.php
+		$.ajax({
+			type: "POST",
+			url:  "upload.php",
+			cache: false,
+			contentType: false, 
+			processData: false, 
+			data: formData,
+			success: function (data) {
+				// for debugging
+				console.log(data);
+				// update the chat area with data from upload.php
+				$("#chatbox").html(data);
+			}
+		});
+	});
+});
+
+// chat area should autoscroll by default
+var autoScroll = true;
+
+// disable autoscroll
+function disableAutoScroll()
+{
+	autoScroll = false;
+}
+
+// enable autoscroll
+function enableAutoScroll()
+{
+	autoScroll = true;
+}
+
+// update chat messages every 0.5s
+setInterval(function(){
+	// use ajax to retrieve data from get.php
+	$.ajax({
+	 	url:  "get.php",
+	 	success: function(data){
+	 		// constantly update the chat area messages
+	 		$("#chatbox").html(data);
+	
+			// if mouse cursor is not over chat area then autoscroll
+	 		if(autoScroll)
+	 		{
+	 			var elem = document.getElementById("chatbox");
+	 			elem.scrollTop = elem.scrollHeight;
+	 		}
+		}
+	});
+}, 500);
+
+// update online users every 0.5s
+setInterval(function(){
+	// use ajax to retrieve data from online.php
+	$.ajax({
+	 	url:  "online.php",
+	 	success: function(data){
+	 		// constantly update online users section
+	 		$("#online").html(data);
+		}
+	});
+}, 500);	
